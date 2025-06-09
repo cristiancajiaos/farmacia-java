d42e90bebfd7fe6f7fac9ab2674de87595b5faa1
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-06-09 10:30:57 -0400

    Creada vista de Login

---
+++ src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import views.LoginView;
+
+public class Main {
+    public static void main(String[] args) {
+        // Instancia del login
+        LoginView login = new LoginView();
+        login.setVisible(true);
+    }
+
+}
