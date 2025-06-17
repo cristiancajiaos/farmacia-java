cc74772217eae9dbd200e7d1434ac66175d939f6
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-06-17 10:12:53 -0400

    Creado método para obtener ID máximo de la venta, formateo de string para los errores en los bloques catch

--- src/models/SalesDAO.java
+++ src/models/SalesDAO.java
@@ -9,19 +9,20 @@ import java.util.Date;
 import javax.swing.JOptionPane;
 
 public class SalesDAO {
-    
+
     // Instanciar la conexión
     ConnectionMySQL cn = new ConnectionMySQL();
     Connection conn;
     PreparedStatement pst;
     ResultSet rs;
-    
+
     // Registrar venta
-    public boolean registerSaleQuery(int customer_id, int employee_id, double total) {
+    public boolean registerSaleQuery(int customer_id, int employee_id,
+            double total) {
         String query = "INSERT INTO sales (customer_id, employee_id, total, "
-                 + "sale_date) VALUES(?, ?, ?, ?);";
+                + "sale_date) VALUES(?, ?, ?, ?)";
         Timestamp datetime = new Timestamp(new Date().getTime());
-        
+
         try {
             conn = cn.getConnection();
             pst = conn.prepareStatement(query);
@@ -32,19 +33,23 @@ public class SalesDAO {
             pst.execute();
             return true;
         } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, "Hubo un error al registrar la venta: " + e.getMessage());
-            System.err.println("Hubo un error al registrar la venta: " + e.getMessage());
+            String strError = "Hubo un error al obtener el ID máximo de la "
+                    + "venta: " + e.getMessage();
+            JOptionPane.showMessageDialog(null, strError);
+            System.err.println(strError);
             return false;
         }
     }
-    
+
     // Registrar detalle de la venta
-    public boolean registerSaleDetailQuery(int product_id, int sale_id, int sale_quantity, double sale_price, double sale_subtotal) {
+    public boolean registerSaleDetailQuery(int product_id, int sale_id,
+            int sale_quantity, double sale_price,
+            double sale_subtotal) {
         String query = "INSERT INTO sales (product_id, sale_id, sale_quantity, "
-                     + "sale_price, sale_subtotal) VALUES (?, ?, ?, ?, ?)";
+                + "sale_price, sale_subtotal) VALUES (?, ?, ?, ?, ?)";
         /* Timestamp no se requiere para registrar el detalle de la venta */
         // Timestamp datetime = new Timestamp(new Date().getTime());
-        
+
         try {
             conn = cn.getConnection();
             pst = conn.prepareStatement(query);
@@ -56,12 +61,33 @@ public class SalesDAO {
             pst.execute();
             return true;
         } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, "Hubo un error al registrar el detalle de la venta: " + e.getMessage());
-            System.err.println("Hubo un error al registrar el detalle de la venta: " + e.getMessage());
+            JOptionPane.showMessageDialog(null, "Hubo un error "
+                    + "al registrar el detalle de la venta: " + e.getMessage());
+            System.err.println("Hubo un error al registrar el detalle "
+                    + "de la venta: " + e.getMessage());
             return false;
         }
-        
+
     }
     
-    
+    // Obtener ID máximo de la venta
+    public int saleId() {
+        int id = 0;
+        String query = "SELECT MAX(id) AS id from sales";
+        
+        try {
+            conn = cn.getConnection();
+            pst = conn.prepareStatement(query);
+            rs = pst.executeQuery();
+            if (rs.next()) {
+                id = rs.getInt("id");
+            }
+        } catch (SQLException e) {
+            String strError = "Hubo un error al obtener el ID máximo de la "
+                    + "venta: " + e.getMessage();
+            JOptionPane.showMessageDialog(null, strError);
+            System.err.println(strError);
+        }
+    }
+
 }
