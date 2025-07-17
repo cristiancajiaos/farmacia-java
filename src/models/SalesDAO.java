d196bb632e55a76e72d2ce4694950b01ae2cc955
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-07-17 12:36:36 -0400

    Corrección en el método para registrar detalle de la venta en la clase salesDAO

--- src/models/SalesDAO.java
+++ src/models/SalesDAO.java
@@ -46,7 +46,7 @@ public class SalesDAO {
     // Registrar detalle de la venta
     public boolean registerSaleDetailQuery(int product_id, int sale_id,
             int sale_quantity, double sale_price, double sale_subtotal) {
-        String query = "INSERT INTO sales (product_id, sale_id, sale_quantity, "
+        String query = "INSERT INTO sale_details (product_id, sale_id, sale_quantity, "
                      + "sale_price, sale_subtotal) VALUES (?, ?, ?, ?, ?)";
         /* Al no ocuparse el parámetro de timestamp, se omite */
 
